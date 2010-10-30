14fe1c837b4ab4b7497814578ccaf1aa3ddc3e2b
Kristoffer Roupé <kitofr@gmail.com>
2010-10-30 20:41:50 +0200

    some specs

---
+++ pentago.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'spec'
+
+describe "a pentago game" do
+	it "should consist of game field with 4 squares and 3x3 bit"
+	it "should be possible to rotate a square left"
+	it "should be possible to rotate a square right"
+	it "should end when 5 balls are in a row"
+	it "should have 2 players"
+	#subjects game_field, square, bit, ball, player
+	#verbs rotate, end
+end
+
