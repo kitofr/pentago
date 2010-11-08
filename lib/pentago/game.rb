519e67b3d186823b19cf4c3775114235a2d4b362
Kristoffer Roupé <kitofr@gmail.com>
2010-11-08 22:53:22 +0100

    Hitting the sack now...

--- lib/pentago/game.rb
+++ lib/pentago/game.rb
@@ -23,7 +23,9 @@ class Game
   def finished?
     tmp  = [@squares[0].zip(@squares[1])]
 		tmp[1] = @squares[2].zip(@squares[3])
-    tmp.flatten.count{ |v| v == 'w' } == 5
+    white = tmp.flatten.count{ |v| v == 'w' }
+    black = tmp.flatten.count{ |v| v == 'b' }
+    white == 5
   end
 	def to_s
 		tmp  = [@squares[0].zip(@squares[1])]
