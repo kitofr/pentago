677830ec942729f59d8a1d20c3bd1c6ce6e78e7f
kitofr <kitofr@gmail.com>
2010-11-21 22:50:42 +0100

    Added test file

---
+++ test.rb
@@ -0,0 +1,56 @@
+
+ 
+puts "-------------lambda--------------"
+skip_6 = lambda { |array, i|
+	@i = @i.nil? ? i : @i + 6 
+	@i = i if @i >= array.length
+	array[@i]
+}
+
+game = (0..35).to_a
+6.times do 
+	6.times do |i|
+		puts "#{i}: #{skip_6.call( game, i)}"
+	end
+	game.shift(1)
+	puts ""
+end
+
+
+puts "-------------yield--------------"
+def skip_six(array, i)
+	@a = i if @a.nil? || @a >= array.length
+	yield array[@a]
+	@a += 6
+end
+
+game = (0..35).to_a
+6.times do
+	6.times do |i|
+		skip_six(game, i) do |v|
+			puts "#{i}: #{v}"
+		end
+	end
+	game.shift(1)
+	puts ""
+end
+
+puts "-------------iterator--------------"
+
+def iterate(iterator)
+	loop {
+		yield iterator.next
+		5.times do iterator.next end
+	}
+end
+
+
+game = (0..35).to_a
+6.times do
+	iterate(game.each) do |x|
+		puts x
+	end
+	game.shift(1)
+	puts ""
+end
+
