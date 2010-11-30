4cd1f386c85cc87c6c3da80705c8a4a2dbcb7102
kitofr <kitofr@gmail.com>
2010-11-30 17:07:23 +0100

    refactored some, don't know if it got me anywhere but...

--- lib/ext/array_ext.rb
+++ lib/ext/array_ext.rb
@@ -3,8 +3,9 @@ class Array
     cnt = 0
     self.each_index do |i|
       item = self[i]
-      cnt = 1 if item != '.' && i == 0
-      cnt = (item != '.' && (item == self[i-1] || self[i-1] == '.')) ? cnt + 1 : 0 if i > 0
+			prev = self[i-1]
+
+			cnt = (item != '.') && (prev && item == prev || prev == '.') ? cnt + 1 : 0
       break if cnt == 5
     end
     cnt == 5
